9347b997a5b13436300f68ca5bd37d5f84a69ac0
Anjali <anjaliap1227@gmail.com>
2024-10-07 16:12:33 +0530

    First commit

---
+++ Program.java
@@ -0,0 +1,11 @@
+public class Program {
+    public static void main(String[] args) {
+        int num[]={14,12,34,4,5,68,7,89,9};
+        System.out.print("Elements of an array in Reverse order: ");
+        for(int i=num.length-1;i>=0;i--){
+            System.out.print(num[i]+" ");
+        }
+        System.out.println("Done");
+        System.out.println("array progrsam");
+    }
+}
